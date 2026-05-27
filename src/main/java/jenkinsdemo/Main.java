76ed4e960daa9c45b6b702c633899e78d826feb7
Echsi <thomaec2006@gmail.com>
2026-05-27 09:17:08 +0200

    test01

--- src/main/java/jenkinsdemo/Main.java
+++ src/main/java/jenkinsdemo/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Demo demo = new Demo();
+        f
 
         System.out.println("add(2, 3) = " + demo.add(2, 3));
         System.out.println("isEven(10) = " + demo.isEven(10));
